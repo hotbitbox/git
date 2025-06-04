84546570c27dc85113fd43dc2b8618c8a07eb551
Krzysztof Skoczyński <krzysztof.skoczynski@trapezegroup.pl>
2025-06-04 09:58:04 +0200

    Pass alias name as parameter

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Default alias name is 'git' if not provided as first parameter
+ALIAS_NAME="${1:-git}"
+
 USER_NAME=$(id -un)
 USER_ID=$(id -u)
 
@@ -8,15 +11,15 @@ docker build \
   --build-arg USER_ID="$USER_ID" \
   -t hotbitbox/git .
 
-  if grep -q "alias git=" ~/.bash_aliases 2>/dev/null; then
-    # Remove existing git alias
-    sed -i '/alias git=/,/^alias /{//!d;}; /alias git=/d' ~/.bash_aliases
-    echo "Removed existing git alias from ~/.bash_aliases"
+  if grep -q "alias $ALIAS_NAME=" ~/.bash_aliases 2>/dev/null; then
+    # Remove existing alias
+    sed -i "/alias $ALIAS_NAME=/,/^alias /{//!d;}; /alias $ALIAS_NAME=/d" ~/.bash_aliases
+    echo "Removed existing $ALIAS_NAME alias from ~/.bash_aliases"
   fi
 
-  echo "alias git='docker run --rm -it \\
+  echo "alias $ALIAS_NAME='docker run --rm -it \\
     -v \$HOME:/home/\$(id -un) \\
     -v \$(pwd):/repo \\
     -u \$(id -u):\$(id -g) \\
     hotbitbox/git'" >> ~/.bash_aliases
-  echo "Added git alias to ~/.bash_aliases"
+  echo "Added $ALIAS_NAME alias to ~/.bash_aliases"
